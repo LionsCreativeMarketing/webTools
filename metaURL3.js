324a3949125ae0a3d173fa294c751fcdd31d7ad7
LionsCreativeMarketing <99049037+LionsCreativeMarketing@users.noreply.github.com>
2022-04-18 14:58:48 -0400

    Update and rename metaURL2.js to metaURL3.js

--- metaURL3.js
+++ metaURL3.js
@@ -1,7 +1,7 @@
 //Paste this code into the head of the project to duplicate on every page.
-//<script src="https://cdn.jsdelivr.net/gh/LionsCreativeMarketing/webTools@main/metaURL2.js"></script>
+//<script src="https://cdn.jsdelivr.net/gh/LionsCreativeMarketing/webTools@main/metaURL3.js"></script>
 //$('head').append('<meta property="og:url" content="'+window.location.href+'">');
 var meta = document.createElement('meta');
-meta.property = "og:url";
-meta.content = ""+window.location.href+"";
+meta.setAttribute('property', 'og:url');
+meta.setAttribute('content', ''+window.location.href+'');
 document.getElementsByTagName('head')[0].appendChild(meta);
